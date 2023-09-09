cf264f58bece2c59b7184c6bcbaae14cd31ed55f
Emre <118940184+Emre033@users.noreply.github.com>
2023-09-09 14:26:00 +0300

    Create kurucukim.js

---
+++ komutlar/kullanici/kurucukim.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name:"kurucu",
+    code:`
+    $desciption[1;Bu Sunucunun Kurucusu [$userTag[$ownerID]](https://discord.com/users/$ownerID) Adlı Kullanıcıdır.]
+    $reply`
+    }
